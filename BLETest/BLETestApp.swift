fbf24525d1b5ea91ece7daac0eb7fa47e3e5e156
Ternovoi <danyloternovoi@gmail.com>
2025-05-12 16:54:49 +0300

    Initial Commit

---
+++ BLETest/BLETestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLETestApp.swift
+//  BLETest
+//
+//  Created by Danylo Ternovoi on 12.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct BLETestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
